5241f9bde5f532f4d007464746e222c83f6bf60e
Aleksey Kosin <akosin@udm.ru>
2023-01-04 17:05:00 +0400

    csrf_trust

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -145,3 +145,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index_page' # URL redirecting after a successful authentication
+
+CSRF_TRUSTED_ORIGINS=["https://python-project-52-production-ba0b.up.railway.app"]
