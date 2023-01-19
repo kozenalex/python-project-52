5bb1bfae40abcdf07fb496324b4c35ba8632c9be
Aleksey Kosin <akosin@udm.ru>
2023-01-19 12:44:36 +0400

    woraruond

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ load_dotenv(dotenv_path=env_path)
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'workaround')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
